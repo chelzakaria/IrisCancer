b14b3fdb4163cfa7960f932d1b65a152fb8d2278
Zakaria Chel <80723047+chelzakaria@users.noreply.github.com>
2023-01-01 20:28:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-#
+import streamlit as st
+import pandas as pd
+
+cancer_data = pd.read_csv('cancer.csv')
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(cancer_data)
